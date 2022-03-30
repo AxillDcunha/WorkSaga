481eab689e1d71070e2dd45814c32b64a5fd5eed
Lisban Gonsalves <63594467+Lisbangonsalves@users.noreply.github.com>
2022-03-30 21:21:45 +0530

    Co-authored-by: Axill Dcunha <AxillDcunha@users.noreply.github.com> Co-authored-by: jAsOnN07 <jAsOnN07@users.noreply.github.com>

--- lib/widgets/homepage_appbar.dart
+++ lib/widgets/homepage_appbar.dart
@@ -1,90 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomepageAppbar extends StatefulWidget {
+class AppBarWidget extends StatefulWidget {
   @override
-  State<HomepageAppbar> createState() => _HomepageAppbarState();
+  _AppBarWidgetState createState() => _AppBarWidgetState();
 }
 
-class _HomepageAppbarState extends State<HomepageAppbar> {
+class _AppBarWidgetState extends State<AppBarWidget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: Colors.white,
-      body: Align(
-        alignment: AlignmentDirectional(-1, -1),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: 120,
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          alignment: AlignmentDirectional(0, 0),
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      flexibleSpace: Container(
+          width: double.infinity,
           child: Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(20, 20, 20, 10),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Text(
-                      'Work Saga',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        print('Button pressed ...');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        shadowColor: Colors.white,
-                        elevation: 0
-                        ),
-                      icon: Icon(
-                        Icons.location_city,
-                        color: Color.fromARGB(255, 0, 0, 0),
-                        size: 18.0,
-                      ),
-                      label: Text('Location', style: TextStyle(color: Colors.black),),
-                    ),
-                  ],
-                ),
-                ElevatedButton.icon(
+            padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(
+                'Work Saga',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'NanumMyeongjo'),
+              ),
+              TextButton.icon(
+                  onPressed: () {},
                   icon: Icon(
-                    Icons.search_sharp,
+                    Icons.location_on,
+                    size: 16,
                     color: Colors.black,
-                    size: 30,
                   ),
-                  style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        shadowColor: Colors.white,
-                        elevation: 0
-                        ),
-                  onPressed: () {
-                    print('IconButton pressed ...');
-                  },
-                  label: Text(''),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-
-
-
-
-
-      
+                  label: Text(
+                    'Location',
+                    style: TextStyle(color: Colors.black),
+                  ))
+            ]),
+          )),
     );
   }
 }
